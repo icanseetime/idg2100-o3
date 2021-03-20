e60cf1b71f4a301d525fc97d65184b3c1666a7a7
Ida <imgjeits@stud.ntnu.no>
2021-03-20 17:30:20 +0100

    added easter egg

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.status(418).send({ teapot: '☕' })
+    res.status(418).send({ tea: '☕', quote: `'Have some wine,' the March Hare said in an encouraging tone. Alice looked around the table, but there was nothing on it but tea. 'I don't see any wine,' she remarked. 'There isn't any,' said the March Hare.` })
 })
 
 module.exports = router
